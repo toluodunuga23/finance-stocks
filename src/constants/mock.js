6369971f1d7b9cd2a432a31137b036a5904ec24d
kawe-education <to379@drexel.edu>
2025-02-01 23:16:07 -0500

    initial commit of mock stocks

---
+++ src/constants/mock.js
@@ -0,0 +1,31 @@
+export const mockStocks=[
+    {
+        stock: 'AAPL',
+        price: 150.29,
+        ipo: '1212',
+        change: 0.12,
+        logo: ''
+    },
+    {
+        stock: 'MSFT',
+        price: 300.29,
+        ipo: '1212',
+        change: 0.12,
+        logo: ''
+    },
+    {
+        stock: 'AMZN',
+        price: 3500.29,
+        ipo: '1212',
+        change: 0.12,
+        logo: ''
+    },
+    {
+        stock: 'TSLA',
+        price: 750.29,
+        ipo: '1212',
+        change: 0.12,
+        logo: ''
+    }
+
+]
